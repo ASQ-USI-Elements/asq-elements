ba5b9954d60fb30d70f68fdbf5b38c127f52543f
youyix <youyis2fox@gmail.com>
2014-12-10 17:42:36 +0100

    examples

---
+++ examples/SamplePresentation/js/init.js
@@ -0,0 +1,32 @@
+function init() {
+  //detect if we need to render anything or the user is just chilin
+  var search = window.location.search;
+  if (search.match(/viewer/)) {
+    setRole("viewer");
+  } else if (search.match(/presenter/)) {
+    setRole("presenter");
+  } else if (typeof cb !== 'undefined' && typeof cb === 'function') {
+    // cb.call(this, null, true);
+  }
+}
+
+function setRole(role) {
+  var allAsqElements = getASQElements();
+  allAsqElements.forEach(function(elem, index) {
+    elem.role = role;
+  });
+}
+
+function getASQElements() {
+  var allAsqElements = document.querySelectorAll('html /deep/ *');
+
+  allAsqElements = Array.prototype.slice.call(allAsqElements).filter(function(el) {
+    return isASQEl(el);
+  });
+
+  return allAsqElements;
+
+  function isASQEl(el) {
+    return el.localName.indexOf('asq-') != -1;
+  } 
+} 
